e5dbd58f9294495da0175904511857aa1809c4da
codeninja17 <ankitkr09@gmail.com>
2021-06-19 12:46:35 +0530

    Initial commit

---
+++ inventory-fail/src/main/java/com/example/inventoryfail/InventoryFailApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryfail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryFailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryFailApplication.class, args);
+	}
+
+}
---
+++ inventory-fail/src/test/java/com/example/inventoryfail/InventoryFailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryfail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryFailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
